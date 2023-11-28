e3b507ff789efa4bb6e290d6103bf70bed37cebb
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2023-11-28 17:29:33 +0200

    feat : auth microservice setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'AUTH_SERVICE',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const RABBITMQ_USER = configService.get('RABBITMQ_USER');
+        const RABBITMQ_PASS = configService.get('RABBITMQ_PASS');
+        const RABBITMQ_HOST = configService.get('RABBITMQ_HOST');
+        const RABBITMQ_AUTH_QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
+
+        return ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASS}@${RABBITMQ_HOST}`],
+            queue: RABBITMQ_AUTH_QUEUE,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        });
+      },
+    },
+  ],
 })
 export class AppModule {}
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const RABBITMQ_USER = configService.get('RABBITMQ_USER');
+  const RABBITMQ_PASS = configService.get('RABBITMQ_PASS');
+  const RABBITMQ_HOST = configService.get('RABBITMQ_HOST');
+  const RABBITMQ_AUTH_QUEUE = configService.get('RABBITMQ_AUTH_QUEUE');
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASS}@${RABBITMQ_HOST}`],
+      queue: RABBITMQ_AUTH_QUEUE,
+      noAck: false,
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
